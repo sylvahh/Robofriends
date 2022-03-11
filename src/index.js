5f939c34b624d8ee1196cdb600be797f9ad2e1ac
sylvahh <sylvesterchambers03@gmail.com>
2022-03-11 13:20:58 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList.js';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots';
 
-ReactDOM.render(
-  <CardList robots={robots} />,
-
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 reportWebVitals();
